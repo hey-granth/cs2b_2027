f0eb686ec67d67da1f6fab346617fed7b601a429
Granth Agarwal <heygranth@gmail.com>
2025-04-28 12:26:20 +0530

    added name to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,32 @@
-import {menuItems} from "../data.js";
+import { menuItems, socialItems } from "../data";
 
 const Footer = () => {
-  return (
-    <footer class="section footer">
-      <ul class="footer-links">
-        {
-          menuItems.map((item)  => (
-              <li>
-                <a href={item.link} className="footer-link">
-                  {item.name}
-                </a>
-              </li>
-          ))
-        }
-      </ul>
-      <ul class="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-squarespace"></i>
-          </a>
-        </li>
-      </ul>
-      <p class="copyright">
-        copyright &copy; Backroads travel tours company |
-        <span id="date"></span>all rights reserved
-      </p>
-    </footer>
-  );
+    return (
+        <footer class="section footer">
+            <ul class="footer-links">
+                {menuItems.map((item) => (
+                    <li>
+                        <a href={item.link} class="footer-link">
+                            {item.name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <ul class="footer-icons">
+                {socialItems.map((item) => (
+                    <li>
+                        <a href={item.link} target="_blank" class="footer-icon">
+                            <i class={item.icon}></i>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <p class="copyright">
+                copyright &copy; Backroads travel tours company
+                <span id="date"></span> all rights reserved
+            </p>
+            <p class="copyright">developed and maintained by Granth Agarwal</p>
+        </footer>
+    );
 };
 export default Footer;
